6f682e237fa1a6baec602c50b1d6d6adf4d31086
Christian Loera <chatoloera18@gmail.com>
2017-12-12 22:27:59 -0600

    Added an install file

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Going to make some installs, hold back and relax"
+sudo apt-get update
+sudo apt-get install bluetooth -y
+sudo apt-get install python-cwiid
+echo "Done installing"
